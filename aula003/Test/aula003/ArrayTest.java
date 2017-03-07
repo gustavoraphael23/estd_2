26172240c083e223dad67db58de36c090d9e3dcf
gustavo.perei <gustavo.perei@LAB-B121-34.univille.br>
2017-03-06 21:52:26 -0300

    test 4

---
+++ aula003/Test/aula003/ArrayTest.java
@@ -0,0 +1,19 @@
+package aula003;
+
+import org.junit.Test;
+
+public class ArrayTest {
+	
+	int i; 
+	
+	Integer y;
+	
+	@Test
+	public void sumTest(){		
+		
+		Array array = new Array();
+		array.test();
+	
+	}
+
+}
---
+++ aula003/src/aula003/Array.java
@@ -0,0 +1,12 @@
+package aula003;
+
+public class Array {
+
+	int i;
+	Integer y; 
+	
+	public void test(){
+		System.out.println(i + y);
+	}
+
+}
